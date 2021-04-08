8a6e7857be34d21c13d2d51dc570f650dd90eadd
Brandon Haley <kyle7286@gmail.com>
2021-04-08 12:48:28 -0400

    Added comments to model association index

--- models/index.js
+++ models/index.js
@@ -1,33 +1,34 @@
+// Import all models
 const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
-// A single user can have many blogs
+
+// A user can have many blogs
 User.hasMany(Blog, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
-
+// A user and have many comments
 User.hasMany(Comment, {
     foreignKey: 'user_id'
 })
-
+// A blog can have many comments
 Blog.hasMany(Comment, {
     foreignKey: 'blog_id',
 })
-
+// A blog belongs to a single user
 Blog.belongsTo(User, {
     foreignKey: 'id',
     onDelete: 'CASCADE'
 });
-
+// A comment belongs to a single user
 Comment.belongsTo(User, {
     foreignKey: 'id'
 })
-
+// A comment belongs to a single blog
 Comment.belongsTo(Blog, {
     foreignKey: 'blog_id'
 })
 
-
-
+// Expore the above associations
 module.exports = { Comment, User, Blog };
